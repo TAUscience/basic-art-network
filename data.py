31b3d6960d7d9478605d9ed09fcb0d16940046ee
MichelBEHDB <145080519+MichelBEHDB@users.noreply.github.com>
2024-11-08 23:28:45 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 from sklearn.datasets import load_wine
-wine_data = load_wine()
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
+wine_data = load_wine()
 primeras_tres_caracteristicas = wine_data.data[:, :3]
 
-datos = np.array([
-    [row[0], row[1], row[2]] for row in primeras_tres_caracteristicas
-])
+scaler = MinMaxScaler(feature_range=(0, 1))
+datos_normalizados = scaler.fit_transform(primeras_tres_caracteristicas)
+
+datos = np.array([row for row in datos_normalizados])
